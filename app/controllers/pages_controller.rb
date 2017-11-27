5771d61e1387d35b76339535de26a2768d669e50
Sam Cordingley <sam@cordandberg.com>
2017-11-27 22:20:56 +1100

    surprise page and function working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def random
+    @menus = Menu.select('name').collect { |m| m.name }
+    @random_selection = @menus.sample
+  end
 end
