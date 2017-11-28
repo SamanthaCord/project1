e84612bc1db29c79de5ffae7aa9cefebfd60686a
Sam Cordingley <sam@cordandberg.com>
2017-11-28 16:11:46 +1100

    weather controls menu display function working, cloudinary holding my API key

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
 
   def weathersearch
     @menus = Menu.all
-    url = "http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=702c2a9934ee7c4b45be4b819910d1c8"
+    url = "http://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=#{ ENV["OPEN_WEATHER_API_KEY"] }"
     @info = HTTParty.get(url);
     @current_temp = @info['main']['temp'].to_i / 10
   end
